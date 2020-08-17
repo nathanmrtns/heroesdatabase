a28f0f03a728bcc05edcefff4f6f995065e4933a
Nathan Barreto <nathanmrtns@gmail.com>
2020-08-16 22:40:52 -0300

    Adding gh-pages (wont work on private repo)

---
+++ build/precache-manifest.59cd3828d75ebc482fffd075be89596b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc9cff7f4ce3e2a3134c33c871e02ce",
+    "url": "/codingchallenge/index.html"
+  },
+  {
+    "revision": "4d7a79e2ab79d8059d5e",
+    "url": "/codingchallenge/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "8dca55aaabd4856c0301",
+    "url": "/codingchallenge/static/css/main.a6cbbe6a.chunk.css"
+  },
+  {
+    "revision": "4d7a79e2ab79d8059d5e",
+    "url": "/codingchallenge/static/js/2.d9a8e5ce.chunk.js"
+  },
+  {
+    "revision": "8dca55aaabd4856c0301",
+    "url": "/codingchallenge/static/js/main.b9cbfd51.chunk.js"
+  },
+  {
+    "revision": "a1148217dd8d2a11d137",
+    "url": "/codingchallenge/static/js/runtime~main.a1effb67.js"
+  }
+]);
