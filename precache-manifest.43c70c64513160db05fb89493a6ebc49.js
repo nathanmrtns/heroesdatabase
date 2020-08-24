346911188e666efe8875b84837738d72a4ddffe3
Nathan Barreto <nathanmrtns@gmail.com>
2020-08-24 11:00:36 -0300

    Updates

---
+++ precache-manifest.43c70c64513160db05fb89493a6ebc49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf01aa785a8cb44aeb3a12066e67525",
+    "url": "/heroesdatabase/index.html"
+  },
+  {
+    "revision": "9550c0ca6a23d4b63e19",
+    "url": "/heroesdatabase/static/css/main.83dd1daa.chunk.css"
+  },
+  {
+    "revision": "a1d8ea9b34fe19968124",
+    "url": "/heroesdatabase/static/js/2.e729f842.chunk.js"
+  },
+  {
+    "revision": "9550c0ca6a23d4b63e19",
+    "url": "/heroesdatabase/static/js/main.60b2720c.chunk.js"
+  },
+  {
+    "revision": "9932c32b9828f446d19e",
+    "url": "/heroesdatabase/static/js/runtime~main.c87e1b25.js"
+  },
+  {
+    "revision": "f4e47a1c1278feb79ab311361e242dc0",
+    "url": "/heroesdatabase/static/media/marvel.f4e47a1c.svg"
+  }
+]);
